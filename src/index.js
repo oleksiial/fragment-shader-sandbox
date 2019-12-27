d5562b0b55a61dc3a6f994cbc2006cd8a392cae3
oleksii <oleksii.al@gmail.com>
2019-12-27 15:17:16 +0100

    Fix whitespaces

--- src/index.js
+++ src/index.js
@@ -5,37 +5,37 @@ import vertexShaderSource from './shaders/shader.vert';
 import fragmentShaderSource from './shaders/rainbow3yoKid.frag';
 
 const main = () => {  
-  const program = createProgramFromShaderSources(gl, vertexShaderSource, fragmentShaderSource);
-  const resolutionLoc = gl.getUniformLocation(program, 'u_resolution');
-  const mouseLoc = gl.getUniformLocation(program, 'u_mouse');
-  const timeLoc = gl.getUniformLocation(program, 'u_time');
+	const program = createProgramFromShaderSources(gl, vertexShaderSource, fragmentShaderSource);
+	const resolutionLoc = gl.getUniformLocation(program, 'u_resolution');
+	const mouseLoc = gl.getUniformLocation(program, 'u_mouse');
+	const timeLoc = gl.getUniformLocation(program, 'u_time');
 
-  const mouse = [0, 0];
-  const rect = gl.canvas.getBoundingClientRect();
-  gl.canvas.addEventListener('mousemove', (e) => {
-    mouse[0] = e.clientX - rect.left;
-    mouse[1] = gl.canvas.height - e.clientY + rect.top;
-  });
+	const mouse = [0, 0];
+	const rect = gl.canvas.getBoundingClientRect();
+	gl.canvas.addEventListener('mousemove', (e) => {
+		mouse[0] = e.clientX - rect.left;
+		mouse[1] = gl.canvas.height - e.clientY + rect.top;
+	});
 
-  const start = Math.floor(Date.now());
-  const drawScene = () => {
-    resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    
-    gl.useProgram(program);
-    gl.uniform2f(resolutionLoc, gl.canvas.width, gl.canvas.height);
-    gl.uniform2f(mouseLoc, mouse[0], mouse[1]);
-    gl.uniform1f(timeLoc, (Date.now() - start) / 1000);
+	const start = Math.floor(Date.now());
+	const drawScene = () => {
+		resizeCanvasToDisplaySize(gl.canvas);
+		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+		gl.clearColor(0, 0, 0, 0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
 
-    requestAnimationFrame(drawScene);
-  }
+		gl.useProgram(program);
+		gl.uniform2f(resolutionLoc, gl.canvas.width, gl.canvas.height);
+		gl.uniform2f(mouseLoc, mouse[0], mouse[1]);
+		gl.uniform1f(timeLoc, (Date.now() - start) / 1000);
 
-  requestAnimationFrame(drawScene);
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+		requestAnimationFrame(drawScene);
+	}
+
+	requestAnimationFrame(drawScene);
 }
 
 document.addEventListener('DOMContentLoaded', main);
--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,45 @@
 const createShader = (gl, type, source) => {
-  const shader = gl.createShader(type);
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
-  }
- 
-  console.log(gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
+	const shader = gl.createShader(type);
+	gl.shaderSource(shader, source);
+	gl.compileShader(shader);
+	const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+	if (success) {
+		return shader;
+	}
+
+	console.log(gl.getShaderInfoLog(shader));
+	gl.deleteShader(shader);
 }
 
 const createProgram = (gl, vertexShader, fragmentShader) => {
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
-  console.log(gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
+	const program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+	if (success) {
+		return program;
+	}
+	console.log(gl.getProgramInfoLog(program));
+	gl.deleteProgram(program);
 }
 
 export const createProgramFromShaderSources = (gl, vertexShaderSrc, fragmentShaderSrc) => {
-  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSrc);
-  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSrc);
-  const program = createProgram(gl, vertexShader, fragmentShader);
-  return program;
+	const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSrc);
+	const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSrc);
+	const program = createProgram(gl, vertexShader, fragmentShader);
+	return program;
 }
 
 export const resizeCanvasToDisplaySize = (canvas) => {
-  const width  = canvas.clientWidth;
-  const height = canvas.clientHeight;
-  if (canvas.width !== width ||  canvas.height !== height) {
-    canvas.width  = width;
-    canvas.height = height;
-    return true;
-  }
-  return false;
+	const width  = canvas.clientWidth;
+	const height = canvas.clientHeight;
+	if (canvas.width !== width ||  canvas.height !== height) {
+		canvas.width  = width;
+		canvas.height = height;
+		return true;
+	}
+	return false;
 }
 
 export const degToRad = (deg) => deg * Math.PI / 180;
